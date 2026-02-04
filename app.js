e22cb3bd4847c20323c0071354eb54ccc2f7fd60
Sanjay <sankir25092007@gmail.com>
2026-02-04 20:54:52 +0530

    eslint spotted error successfully and changed the app.js back to og

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Server running")
-}) 
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Server is running');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
 
 server.listen(5000, () => {
-  console.log("Started")
-})x   // ← extra character causes crash
-
+  console.log('Server running at http://localhost:5000');
+});
