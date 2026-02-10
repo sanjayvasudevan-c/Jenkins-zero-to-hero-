439e6dc0010f8dbda66172394f60bfcfef5c0cac
Sanjay <sankir25092007@gmail.com>
2026-02-10 13:12:08 +0530

    wnukfb

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
-})x
+});
 
 server.listen(5000, () => {
   console.log('Server running at http://localhost:5000');
